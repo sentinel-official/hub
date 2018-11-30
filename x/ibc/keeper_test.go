bfe7ba5d04735b9cc89483d5639e634b5f318ed4
baymax19 <baymax19@protonmail.com>
2018-11-30 15:53:51 +0530

    testcases for IBC modules

--- x/ibc/keeper_test.go
+++ x/ibc/keeper_test.go
@@ -3,7 +3,7 @@ package ibc
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	csdkTypes "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ironman0x7b2/sentinel-sdk/x/vpn"
+	sdkTypes "github.com/ironman0x7b2/sentinel-sdk/types"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
@@ -13,17 +13,42 @@ import (
 func TestIBC(t *testing.T) {
 
 	cdc := codec.New()
-	cdc.RegisterConcrete(vpn.MsgRegisterNode{},"test/ibc/msg_register_node",nil)
-	multiStore, vpnKey, _, ibcKey, sessionKey := DefaultSetup()
-	ctx := csdkTypes.NewContext(multiStore, abci.Header{}, false, log.NewNopLogger())
+	cdc.RegisterConcrete(sdkTypes.IBCPacket{}, "test/ibc", nil)
+	cdc.RegisterInterface((*sdkTypes.Interface)(nil), nil)
 
-	vpnKeeper := vpn.NewKeeper(cdc, vpnKey, sessionKey)
+	multiStore, ibcKey := DefaultSetup()
+	ctx := csdkTypes.NewContext(multiStore, abci.Header{}, false, log.NewNopLogger())
 	ibcKeeper := NewKeeper(ibcKey, cdc)
 
-	vpnHandler := vpn.NewHandler(vpnKeeper, ibcKeeper)
+	ibcPacket1 := TestNewIBCPacket()
+	ibcPacketRes1 := ibcKeeper.PostIBCPacket(ctx, ibcPacket1)
+	require.Nil(t, ibcPacketRes1)
 
-	msgRegisterNode := TestNewMsgRegisterNode()
-	msgRegisterNodeRes := vpnHandler(ctx, msgRegisterNode)
+	getIBCPacket1, err := ibcKeeper.GetIBCPacket(ctx, EgressKey(ibcPacket1.DestChainID, int64(0)))
+	require.Nil(t, err)
+	require.Equal(t, getIBCPacket1, &ibcPacket1)
+	require.Equal(t, getIBCPacket1.DestChainID, ibcPacket1.DestChainID)
 
-	require.True(t, msgRegisterNodeRes.IsOK())
+	err = ibcKeeper.SetIngressLength(ctx, IngressLengthKey(ibcPacket1.SrcChainID), int64(1))
+	require.Nil(t, err)
+
+	getIBCPacket2, err := ibcKeeper.GetIBCPacket(ctx, EgressKey(ibcPacket1.DestChainID, int64(1)))
+	require.Nil(t, getIBCPacket2)
+
+	err = ibcKeeper.SetEgressLength(ctx, EgressLengthKey(ibcPacket1.DestChainID), int64(1))
+	require.Nil(t, err)
+
+	ibcPacket2 := TestNewIBCPacket()
+	ibcPacketRes2 := ibcKeeper.PostIBCPacket(ctx, ibcPacket2)
+	require.Nil(t, ibcPacketRes2)
+
+	egressLength1, err := ibcKeeper.GetEgressLength(ctx, EgressLengthKey(ibcPacket1.DestChainID))
+	require.Nil(t, err)
+
+	require.Equal(t, int64(2), egressLength1)
+
+	ingressLength1, err := ibcKeeper.GetIngressLength(ctx, IngressLengthKey(ibcPacket1.SrcChainID))
+	require.Nil(t, err)
+	require.Equal(t, int64(1), ingressLength1)
+
 }
