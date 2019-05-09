2ea38dbd6094a36e8b906e90559c5dd929887223
Tony Stark <ironman0x7b2@protonmail.com>
2019-05-09 12:53:59 +0530

    Updated to Cosmos SDK v0.34.4 (#92)

--- apps/hub/export.go
+++ apps/hub/export.go
@@ -74,13 +74,14 @@ func (app *Hub) prepForZeroHeightGenesis(ctx csdkTypes.Context, jailWhiteList []
 	app.assertRuntimeInvariantsOnContext(ctx)
 
 	app.stakingKeeper.IterateValidators(ctx, func(_ int64, val csdkTypes.Validator) (stop bool) {
-		_ = app.distributionKeeper.WithdrawValidatorCommission(ctx, val.GetOperator())
+		_, _ = app.distributionKeeper.WithdrawValidatorCommission(ctx, val.GetOperator())
 		return false
 	})
 
 	dels := app.stakingKeeper.GetAllDelegations(ctx)
 	for _, delegation := range dels {
-		_ = app.distributionKeeper.WithdrawDelegationRewards(ctx, delegation.DelegatorAddress, delegation.ValidatorAddress)
+		_, _ = app.distributionKeeper.WithdrawDelegationRewards(ctx, delegation.DelegatorAddress,
+			delegation.ValidatorAddress)
 	}
 
 	app.distributionKeeper.DeleteAllValidatorSlashEvents(ctx)
