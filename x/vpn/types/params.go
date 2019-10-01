cb74fca047e78742d74e3e51d84b18d90fc2a6cb
baymax19 <baymax19@protonmail.com>
2019-10-01 13:06:04 +0530

    Adding genesis validations for vpn params.

--- x/vpn/types/params.go
+++ x/vpn/types/params.go
@@ -67,3 +67,19 @@ func DefaultParams() Params {
 		SessionInactiveInterval: DefaultSessionInactiveInterval,
 	}
 }
+
+func (p Params) Validate() error {
+	if !p.Deposit.IsValid() {
+		return fmt.Errorf("Deposit is invalid: %s ", p.Deposit.String())
+	}
+
+	if p.NodeInactiveInterval < 0 {
+		return fmt.Errorf("NodeInactiveInterval: %d should be postive interger", p.NodeInactiveInterval)
+	}
+
+	if p.SessionInactiveInterval < 0 {
+		return fmt.Errorf("SessionInactiveInterval: %d should be postive interget", p.SessionInactiveInterval)
+	}
+
+	return nil
+}
