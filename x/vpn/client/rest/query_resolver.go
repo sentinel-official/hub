8716a03e9898cb3f2111d07753d42cc14e8ba562
baymax19 <baymax19@protonmail.com>
2019-12-09 15:25:06 +0530

    Adding rest query for resolver.

---
+++ x/vpn/client/rest/query_resolver.go
@@ -0,0 +1,21 @@
+package rest
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/sentinel-official/hub/x/vpn/client/common"
+	"net/http"
+)
+
+func getResolversHandlerFunc(ctx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		address := r.URL.Query().Get("address")
+
+		resolvers, err := common.QueryResolvers(ctx, address)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		rest.PostProcessResponse(w, ctx, resolvers)
+	}
+}
