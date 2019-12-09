917753f469729107b8237fe5cbd03f64b855d1c5
baymax19 <baymax19@protonmail.com>
2019-12-09 16:26:08 +0530

    Adding rest transactions for resolver.

--- x/vpn/client/common/resolver.go
+++ x/vpn/client/common/resolver.go
@@ -34,6 +34,6 @@ func QueryResolvers(ctx context.CLIContext, address string) (types.Resolvers, er
 	if err := ctx.Codec.UnmarshalJSON(res, &resolvers); err != nil {
 		return nil, err
 	}
-	
+
 	return resolvers, nil
 }
