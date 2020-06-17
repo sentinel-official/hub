52ee9c705a631316e0d21d06e9c83e052aacd0ab
Tony Stark <ironman0x7b2@protonmail.com>
2020-06-17 16:51:45 +0530

    Updated register provider handler

--- x/dvpn/provider/handler.go
+++ x/dvpn/provider/handler.go
@@ -1,12 +1,35 @@
 package provider
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	hub "github.com/sentinel-official/hub/types"
 	"github.com/sentinel-official/hub/x/dvpn/provider/keeper"
 	"github.com/sentinel-official/hub/x/dvpn/provider/types"
 )
 
 func HandleRegisterProvider(ctx sdk.Context, k keeper.Keeper, msg types.MsgRegisterProvider) sdk.Result {
-	return sdk.Result{}
+	i := k.GetProvidersCount(ctx) + 1
+	provider := types.Provider{
+		ID:          hub.NewProviderID(i),
+		Name:        msg.Name,
+		Website:     msg.Website,
+		Description: msg.Description,
+	}
+
+	k.SetProvidersCount(ctx, i)
+	ctx.EventManager().EmitEvent(sdk.NewEvent(
+		types.EventTypeSetProvidersCount,
+		sdk.NewAttribute(types.AttributeKeyProvidersCount, fmt.Sprintf("%d", i)),
+	))
+
+	k.SetProvider(ctx, provider)
+	ctx.EventManager().EmitEvent(sdk.NewEvent(
+		types.EventTypeSetProvider,
+		sdk.NewAttribute(types.AttributeKeyProviderID, provider.ID.String()),
+	))
+
+	return sdk.Result{Events: ctx.EventManager().Events()}
 }
--- x/dvpn/provider/types/events.go
+++ x/dvpn/provider/types/events.go
@@ -1,1 +1,11 @@
 package types
+
+const (
+	EventTypeSetProvidersCount = "set_providers_count"
+	EventTypeSetProvider       = "set_provider"
+)
+
+const (
+	AttributeKeyProvidersCount = "providers_count"
+	AttributeKeyProviderID     = "provider_id"
+)
