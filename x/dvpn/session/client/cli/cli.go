9ea1c12a40fc961476869b4286e003df8b1d2468
Tony Stark <ironman0x7b2@protonmail.com>
2020-06-29 00:13:28 +0530

    Added session CLI transactions

--- x/dvpn/session/client/cli/cli.go
+++ x/dvpn/session/client/cli/cli.go
@@ -1,1 +1,24 @@
 package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/spf13/cobra"
+)
+
+func GetQueryCommands(cdc *codec.Codec) []*cobra.Command {
+	return nil
+}
+
+func GetTxCommands(cdc *codec.Codec) []*cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "session",
+		Short: "Session module sub-commands",
+	}
+
+	cmd.AddCommand(client.PostCommands(
+		txUpdateSession(cdc),
+	)...)
+
+	return []*cobra.Command{cmd}
+}
--- x/dvpn/session/client/cli/tx.go
+++ x/dvpn/session/client/cli/tx.go
@@ -1,1 +1,51 @@
 package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/spf13/cobra"
+
+	hub "github.com/sentinel-official/hub/types"
+	"github.com/sentinel-official/hub/x/dvpn/session/types"
+)
+
+func txUpdateSession(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "update",
+		Short: "Update a session",
+		Args:  cobra.ExactArgs(4),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			txb := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+			ctx := context.NewCLIContext().WithCodec(cdc)
+
+			subscription, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			address, err := sdk.AccAddressFromBech32(args[1])
+			if err != nil {
+				return err
+			}
+
+			upload, err := strconv.ParseInt(args[2], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			download, err := strconv.ParseInt(args[3], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUpdateSession(ctx.FromAddress.Bytes(),
+				subscription, address, hub.NewBandwidthFromInt64(upload, download))
+			return utils.GenerateOrBroadcastMsgs(ctx, txb, []sdk.Msg{msg})
+		},
+	}
+}
