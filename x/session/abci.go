85a56a222cda490b3c1af1512fae48a7747d6560
Tony Stark <ironman0x7b2@protonmail.com>
2020-09-21 01:00:18 +0530

    Fix updation of quota in plan based subscription

--- x/session/abci.go
+++ x/session/abci.go
@@ -15,7 +15,7 @@ func EndBlock(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
 		k.Logger(ctx).Info("Inactive session", "id", item.ID,
 			"subscription", item.Subscription, "node", item.Node, "address", item.Address)
 
-		if err := k.Pay(ctx, item); err != nil {
+		if err := process(ctx, k, item); err != nil {
 			panic(err)
 		}
 
@@ -28,6 +28,60 @@ func EndBlock(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
 
 		return false
 	})
+
+	return nil
+}
+
+func process(ctx sdk.Context, k keeper.Keeper, session types.Session) sdk.Error {
+	subscription, found := k.GetSubscription(ctx, session.Subscription)
+	if !found {
+		return types.ErrorSubscriptionDoesNotExit()
+	}
+
+	quota, found := k.GetQuota(ctx, session.Subscription, session.Address)
+	if !found {
+		return types.ErrorQuotaDoesNotExist()
+	}
+
+	free := quota.Allocated.Sub(quota.Consumed)
+	if !free.IsPositive() {
+		return nil
+	}
+
+	if subscription.Plan == 0 {
+		if subscription.Status.Equal(hub.StatusInactive) {
+			return nil
+		}
+
+		bandwidth := hub.NewBandwidth(
+			session.Bandwidth.Sum(), sdk.ZeroInt(),
+		).CeilTo(
+			hub.Gigabyte.Quo(subscription.Price.Amount),
+		).Sum()
+		if bandwidth.GT(free) {
+			bandwidth = free
+		}
+
+		quota.Consumed = quota.Consumed.Add(bandwidth)
+		k.SetQuota(ctx, session.Subscription, quota)
+
+		amount := subscription.Amount(bandwidth)
+		ctx.Logger().Info("", "price", subscription.Price, "deposit", subscription.Deposit,
+			"consumed", session.Bandwidth.Sum(), "rounded", bandwidth, "amount", amount)
+
+		return k.SendCoinsFromDepositToAccount(ctx, session.Address, session.Node.Bytes(), amount)
+	}
+
+	bandwidth := session.Bandwidth.Sum()
+	if bandwidth.GT(free) {
+		bandwidth = free
+	}
+
+	quota.Consumed = quota.Consumed.Add(bandwidth)
+	k.SetQuota(ctx, session.Subscription, quota)
+
+	ctx.Logger().Info("", "plan", subscription.Plan,
+		"consumed", session.Bandwidth.Sum(), "rounded", bandwidth)
 
 	return nil
 }
