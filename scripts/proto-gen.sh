7afea977f70b4c9c02d3579f43695e21c0e79bed
Srinivas Baride <srini@exidio.co>
2021-02-22 20:03:30 +0530

    Moved all protofiles to directory proto

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -6,17 +6,15 @@ directories=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirn
 for directory in ${directories}; do
   files=$(find "${directory}" -maxdepth 1 -name '*.proto')
   for file in ${files}; do
-    protoc \
-      --proto_path=. \
-      --proto_path="${GOPATH}/src" \
+    buf protoc \
+      --proto_path="proto" \
       --proto_path="${GOPATH}/src/github.com/cosmos/cosmos-sdk/proto" \
       --proto_path="${GOPATH}/src/github.com/cosmos/cosmos-sdk/third_party/proto" \
       --gocosmos_out="plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:${GOPATH}/src" \
       "${file}"
 
-    protoc \
-      --proto_path=. \
-      --proto_path="${GOPATH}/src" \
+    buf protoc \
+      --proto_path="proto" \
       --proto_path="${GOPATH}/src/github.com/cosmos/cosmos-sdk/proto" \
       --proto_path="${GOPATH}/src/github.com/cosmos/cosmos-sdk/third_party/proto" \
       --grpc-gateway_out="logtostderr=true:${GOPATH}/src" \
