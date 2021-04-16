603ec45c32c22e574b1ed510f34518082ebe6f07
Srinivas Baride <srini@exidio.co>
2021-04-16 23:59:33 +0530

    Added migration functions for module plan

---
+++ x/plan/types/legacy/v0.6/plan.go
@@ -0,0 +1,19 @@
+package v0_6
+
+import (
+	hubtypes "github.com/sentinel-official/hub/types/legacy/v0.6"
+	"github.com/sentinel-official/hub/x/plan/types"
+	legacy "github.com/sentinel-official/hub/x/plan/types/legacy/v0.5"
+)
+
+func MigratePlan(item legacy.Plan) types.Plan {
+	return types.Plan{
+		Id:       item.ID,
+		Provider: item.Provider.String(),
+		Price:    item.Price,
+		Validity: item.Validity,
+		Bytes:    item.Bytes,
+		Status:   hubtypes.MigrateStatus(item.Status),
+		StatusAt: item.StatusAt,
+	}
+}
