a61b4fdf212735422c08763ce3d70c53177b644a
Srinivas Baride <srini@exidio.co>
2021-04-17 00:00:32 +0530

    Added migration functions for module VPN

---
+++ x/vpn/types/legacy/v0.5/genesis.go
@@ -0,0 +1,21 @@
+package v0_5
+
+import (
+	deposit "github.com/sentinel-official/hub/x/deposit/types/legacy/v0.5"
+	node "github.com/sentinel-official/hub/x/node/types/legacy/v0.5"
+	plan "github.com/sentinel-official/hub/x/plan/types/legacy/v0.5"
+	provider "github.com/sentinel-official/hub/x/provider/types/legacy/v0.5"
+	session "github.com/sentinel-official/hub/x/session/types/legacy/v0.5"
+	subscription "github.com/sentinel-official/hub/x/subscription/types/legacy/v0.5"
+)
+
+type (
+	GenesisState struct {
+		Deposits      deposit.GenesisState      `json:"deposits"`
+		Providers     provider.GenesisState     `json:"providers"`
+		Nodes         node.GenesisState         `json:"nodes"`
+		Plans         plan.GenesisState         `json:"plans"`
+		Subscriptions subscription.GenesisState `json:"subscriptions"`
+		Sessions      session.GenesisState      `json:"sessions"`
+	}
+)
