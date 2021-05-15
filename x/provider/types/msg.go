450e3643bf83e91d52c53c6a7af88df8af642256
Jasdeep Singh <jasdeepsingh.uppal@gmail.com>
2021-05-16 00:51:36 +0530

    Added test scripts for the module provider (#99)
    * Added test scripts for Provider module
    
    Signed-off-by: jay-dee7 <jasdeep@exidio.co>
    
    * Removed unnecessary changes
    
    Co-authored-by: jay-dee7 <jasdeep@exidio.co>
    Co-authored-by: Srinivas Baride <srini@exidio.co>

--- x/provider/types/msg.go
+++ x/provider/types/msg.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/errors"
-
 	hubtypes "github.com/sentinel-official/hub/types"
 )
 
@@ -34,27 +32,27 @@ func (m *MsgRegisterRequest) Type() string {
 
 func (m *MsgRegisterRequest) ValidateBasic() error {
 	if _, err := sdk.AccAddressFromBech32(m.From); err != nil {
-		return errors.Wrapf(ErrorInvalidField, "%s", "from")
+		return ErrorInvalidFieldFrom
 	}
 
 	// Name length should be between 1 and 64
 	if len(m.Name) == 0 || len(m.Name) > 64 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "name")
+		return ErrorInvalidFieldName
 	}
 
 	// Identity length should be between 0 and 64
 	if len(m.Identity) > 64 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "identity")
+		return ErrorInvalidFieldIdentity
 	}
 
 	// Website length should be between 0 and 64
 	if len(m.Website) > 64 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "website")
+		return ErrorInvalidFieldWebsite
 	}
 
 	// Description length should be between 0 and 256
 	if len(m.Description) > 256 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "description")
+		return ErrorInvalidFieldDescription
 	}
 
 	return nil
@@ -91,29 +89,29 @@ func (m *MsgUpdateRequest) Type() string {
 	return fmt.Sprintf("%s:update", ModuleName)
 }
 
-func (m *MsgUpdateRequest) ValidateBasic() error {
+func (m MsgUpdateRequest) ValidateBasic() error {
 	if _, err := hubtypes.ProvAddressFromBech32(m.From); err != nil {
-		return errors.Wrapf(ErrorInvalidField, "%s", "from")
+		return ErrorInvalidFieldFrom
 	}
 
 	// Name length should be between 0 and 64
 	if len(m.Name) > 64 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "name")
+		return ErrorInvalidFieldName
 	}
 
 	// Identity length should be between 0 and 64
 	if len(m.Identity) > 64 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "identity")
+		return ErrorInvalidFieldIdentity
 	}
 
 	// Website length should be between 0 and 64
 	if len(m.Website) > 64 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "website")
+		return ErrorInvalidFieldWebsite
 	}
 
 	// Description length should be between 0 and 256
 	if len(m.Description) > 256 {
-		return errors.Wrapf(ErrorInvalidField, "%s", "description")
+		return ErrorInvalidFieldDescription
 	}
 
 	return nil
