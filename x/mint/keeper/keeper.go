d912ba90fcaa30059127c94da2c4659fefca1f1e
Srinivas Baride <srini@exidio.co>
2021-09-20 15:35:54 +0530

    Added custommint keeper

---
+++ x/mint/keeper/keeper.go
@@ -0,0 +1,32 @@
+package keeper
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/sentinel-official/hub/x/mint/expected"
+	"github.com/sentinel-official/hub/x/mint/types"
+)
+
+type Keeper struct {
+	cdc  codec.BinaryMarshaler
+	key  sdk.StoreKey
+	mint expected.MintKeeper
+}
+
+func NewKeeper(cdc codec.BinaryMarshaler, key sdk.StoreKey, mint expected.MintKeeper) Keeper {
+	return Keeper{
+		cdc:  cdc,
+		key:  key,
+		mint: mint,
+	}
+}
+
+func (k *Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", "x/"+types.ModuleName)
+}
+
+func (k *Keeper) Store(ctx sdk.Context) sdk.KVStore {
+	return ctx.KVStore(k.key)
+}
