bc010525cf279414cdbda8ae740dddf97385e129
Srinivas Baride <srini@exidio.co>
2022-10-29 16:55:27 +0530

    Optimized the updation of node prices

--- x/node/abci.go
+++ x/node/abci.go
@@ -13,42 +13,47 @@ func EndBlock(ctx sdk.Context, k keeper.Keeper) []abcitypes.ValidatorUpdate {
 	var (
 		log              = k.Logger(ctx)
 		inactiveDuration = k.InactiveDuration(ctx)
+		maxPriceModified = k.IsMaxPriceModified(ctx)
+		minPriceModified = k.IsMinPriceModified(ctx)
 	)
 
-	if k.IsMaxPriceModified(ctx) {
-		price := k.MaxPrice(ctx)
+	if maxPriceModified || minPriceModified {
+		var (
+			maxPrice = sdk.NewCoins()
+			minPrice = sdk.NewCoins()
+		)
+
+		if maxPriceModified {
+			maxPrice = k.MaxPrice(ctx)
+		}
+		if minPriceModified {
+			minPrice = k.MinPrice(ctx)
+		}
+
 		k.IterateNodes(ctx, func(_ int, item types.Node) (stop bool) {
 			if item.Price == nil {
 				return false
 			}
 
-			for _, coin := range price {
-				amount := item.Price.AmountOf(coin.Denom)
-				if amount.GT(coin.Amount) {
-					item.Price = item.Price.Add(
-						sdk.NewCoin(coin.Denom, amount.Neg()),
-					).Add(coin)
+			if maxPriceModified {
+				for _, coin := range maxPrice {
+					amount := item.Price.AmountOf(coin.Denom)
+					if amount.GT(coin.Amount) {
+						item.Price = item.Price.Add(
+							sdk.NewCoin(coin.Denom, amount.Neg()),
+						).Add(coin)
+					}
 				}
 			}
 
-			k.SetNode(ctx, item)
-			return false
-		})
-	}
-
-	if k.IsMinPriceModified(ctx) {
-		price := k.MinPrice(ctx)
-		k.IterateNodes(ctx, func(_ int, item types.Node) (stop bool) {
-			if item.Price == nil {
-				return false
-			}
-
-			for _, coin := range price {
-				amount := item.Price.AmountOf(coin.Denom)
-				if amount.LT(coin.Amount) {
-					item.Price = item.Price.Add(
-						sdk.NewCoin(coin.Denom, amount.Neg()),
-					).Add(coin)
+			if minPriceModified {
+				for _, coin := range minPrice {
+					amount := item.Price.AmountOf(coin.Denom)
+					if amount.LT(coin.Amount) {
+						item.Price = item.Price.Add(
+							sdk.NewCoin(coin.Denom, amount.Neg()),
+						).Add(coin)
+					}
 				}
 			}
 
