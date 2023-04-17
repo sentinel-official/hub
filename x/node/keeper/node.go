1dd12cf462ccb9e9c23b18b5c90fadeb803f7930
Srinivas Baride <srini@exidio.co>
2023-04-17 19:53:55 +0530

    Moved node for plan methods to node file

--- x/node/keeper/node.go
+++ x/node/keeper/node.go
@@ -198,3 +198,59 @@ func (k *Keeper) IterateInactiveNodesAt(ctx sdk.Context, at time.Time, fn func(i
 		i++
 	}
 }
+
+func (k *Keeper) SetActiveNodeForPlan(ctx sdk.Context, id uint64, addr hubtypes.NodeAddress) {
+	var (
+		store = k.Store(ctx)
+		key   = types.ActiveNodeForPlanKey(id, addr)
+		value = k.cdc.MustMarshal(&protobuf.BoolValue{Value: true})
+	)
+
+	store.Set(key, value)
+}
+
+func (k *Keeper) HasActiveNodeForPlan(ctx sdk.Context, id uint64, addr hubtypes.NodeAddress) bool {
+	var (
+		store = k.Store(ctx)
+		key   = types.ActiveNodeForPlanKey(id, addr)
+	)
+
+	return store.Has(key)
+}
+
+func (k *Keeper) DeleteActiveNodeForPlan(ctx sdk.Context, id uint64, addr hubtypes.NodeAddress) {
+	var (
+		store = k.Store(ctx)
+		key   = types.ActiveNodeForPlanKey(id, addr)
+	)
+
+	store.Delete(key)
+}
+
+func (k *Keeper) SetInactiveNodeForPlan(ctx sdk.Context, id uint64, addr hubtypes.NodeAddress) {
+	var (
+		store = k.Store(ctx)
+		key   = types.InactiveNodeForPlanKey(id, addr)
+		value = k.cdc.MustMarshal(&protobuf.BoolValue{Value: true})
+	)
+
+	store.Set(key, value)
+}
+
+func (k *Keeper) HasInactiveNodeForPlan(ctx sdk.Context, id uint64, addr hubtypes.NodeAddress) bool {
+	var (
+		store = k.Store(ctx)
+		key   = types.InactiveNodeForPlanKey(id, addr)
+	)
+
+	return store.Has(key)
+}
+
+func (k *Keeper) DeleteInactiveNodeForPlan(ctx sdk.Context, id uint64, addr hubtypes.NodeAddress) {
+	var (
+		store = k.Store(ctx)
+		key   = types.InactiveNodeForPlanKey(id, addr)
+	)
+
+	store.Delete(key)
+}
