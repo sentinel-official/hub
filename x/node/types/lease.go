1a4cdb5ea7f063b57c022e8f8d687ed80602ade3
Srinivas Baride <srini@exidio.co>
2023-04-18 19:44:39 +0530

    Minor proto generated file changes

--- x/node/types/lease.go
+++ x/node/types/lease.go
@@ -3,7 +3,7 @@ package types
 import (
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/pkg/errors"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 	hubtypes "github.com/sentinel-official/hub/types"
 )
 
