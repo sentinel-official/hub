b1d77508af964957a2ef0da239c15e87942d1e53
Srinivas Baride <srini@exidio.co>
2023-08-24 19:21:59 +0530

    Fixed app store keys

--- app/store.go
+++ app/store.go
@@ -2,16 +2,21 @@ package app
 
 import (
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	authzkeeper "github.com/cosmos/cosmos-sdk/x/authz/keeper"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	evidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
 	"github.com/cosmos/cosmos-sdk/x/feegrant"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/cosmos/cosmos-sdk/x/group"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	nftkeeper "github.com/cosmos/cosmos-sdk/x/nft/keeper"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
@@ -20,7 +25,7 @@ import (
 	ibcicahosttypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/host/types"
 	ibcfeetypes "github.com/cosmos/ibc-go/v7/modules/apps/29-fee/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
-	ibchost "github.com/cosmos/ibc-go/v7/modules/core/24-host"
+	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
 
 	customminttypes "github.com/sentinel-official/hub/x/mint/types"
 	swaptypes "github.com/sentinel-official/hub/x/swap/types"
@@ -28,9 +33,9 @@ import (
 )
 
 type StoreKeys struct {
-	kv        map[string]*sdk.KVStoreKey
-	memory    map[string]*sdk.MemoryStoreKey
-	transient map[string]*sdk.TransientStoreKey
+	kv        map[string]*storetypes.KVStoreKey
+	memory    map[string]*storetypes.MemoryStoreKey
+	transient map[string]*storetypes.TransientStoreKey
 }
 
 func NewStoreKeys() StoreKeys {
@@ -41,19 +46,23 @@ func NewStoreKeys() StoreKeys {
 			authzkeeper.StoreKey,
 			banktypes.StoreKey,
 			capabilitytypes.StoreKey,
+			consensustypes.StoreKey,
+			crisistypes.StoreKey,
 			distributiontypes.StoreKey,
 			evidencetypes.StoreKey,
 			feegrant.StoreKey,
 			govtypes.StoreKey,
+			group.StoreKey,
 			minttypes.StoreKey,
+			nftkeeper.StoreKey,
 			paramstypes.StoreKey,
 			slashingtypes.StoreKey,
 			stakingtypes.StoreKey,
 			upgradetypes.StoreKey,
 
 			// Cosmos IBC keys
+			ibcexported.StoreKey,
 			ibcfeetypes.StoreKey,
-			ibchost.StoreKey,
 			ibcicacontrollertypes.StoreKey,
 			ibcicahosttypes.StoreKey,
 			ibctransfertypes.StoreKey,
@@ -81,10 +90,10 @@ func NewStoreKeys() StoreKeys {
 	}
 }
 
-func (sk *StoreKeys) KVKeys() map[string]*sdk.KVStoreKey               { return sk.kv }
-func (sk *StoreKeys) MemoryKeys() map[string]*sdk.MemoryStoreKey       { return sk.memory }
-func (sk *StoreKeys) TransientKeys() map[string]*sdk.TransientStoreKey { return sk.transient }
+func (sk *StoreKeys) KVKeys() map[string]*storetypes.KVStoreKey               { return sk.kv }
+func (sk *StoreKeys) MemoryKeys() map[string]*storetypes.MemoryStoreKey       { return sk.memory }
+func (sk *StoreKeys) TransientKeys() map[string]*storetypes.TransientStoreKey { return sk.transient }
 
-func (sk *StoreKeys) KV(v string) *sdk.KVStoreKey               { return sk.kv[v] }
-func (sk *StoreKeys) Memory(v string) *sdk.MemoryStoreKey       { return sk.memory[v] }
-func (sk *StoreKeys) Transient(v string) *sdk.TransientStoreKey { return sk.transient[v] }
+func (sk *StoreKeys) KV(v string) *storetypes.KVStoreKey               { return sk.kv[v] }
+func (sk *StoreKeys) Memory(v string) *storetypes.MemoryStoreKey       { return sk.memory[v] }
+func (sk *StoreKeys) Transient(v string) *storetypes.TransientStoreKey { return sk.transient[v] }
