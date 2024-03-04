e85454a81c268e95b396c6a722fb8b47b061c5c4
Srinivas Baride <srini@exidio.co>
2024-03-04 21:17:51 +0530

    feat: complete redelegations and unbondings of an account

--- app/upgrade.go
+++ app/upgrade.go
@@ -4,6 +4,8 @@ package app
 
 import (
 	"fmt"
+	"math"
+	"time"
 
 	sdkmath "cosmossdk.io/math"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
@@ -24,6 +26,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/nft"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
@@ -141,6 +144,21 @@ func UpgradeHandler(
 			return nil, err
 		}
 
+		accAddr, err := sdk.AccAddressFromBech32("")
+		if err != nil {
+			return nil, err
+		}
+
+		if err := completeAllRedelegations(ctx, keepers.StakingKeeper, accAddr, ctx.BlockTime()); err != nil {
+			return nil, err
+		}
+		if err := undelegateAllDelegations(ctx, keepers.StakingKeeper, accAddr); err != nil {
+			return nil, err
+		}
+		if err := completeAllUnbondingDelegations(ctx, keepers.StakingKeeper, accAddr, ctx.BlockTime()); err != nil {
+			return nil, err
+		}
+
 		return newVM, nil
 	}
 }
@@ -164,6 +182,105 @@ func deleteInactiveSubscriptionsForAccounts(ctx sdk.Context, k subscriptionkeepe
 		}
 
 		k.DeleteSubscriptionForAccount(ctx, accAddr, id)
+	}
+
+	return nil
+}
+
+func completeAllRedelegations(ctx sdk.Context, k *stakingkeeper.Keeper, accAddr sdk.AccAddress, completionTime time.Time) error {
+	for _, item := range k.GetRedelegations(ctx, accAddr, math.MaxInt16) {
+		for i := range item.Entries {
+			item.Entries[i].CompletionTime = completionTime
+		}
+
+		k.SetRedelegation(ctx, item)
+
+		fromAddr, err := sdk.ValAddressFromBech32(item.ValidatorSrcAddress)
+		if err != nil {
+			return err
+		}
+
+		toAddr, err := sdk.ValAddressFromBech32(item.ValidatorDstAddress)
+		if err != nil {
+			return err
+		}
+
+		balances, err := k.CompleteRedelegation(ctx, accAddr, fromAddr, toAddr)
+		if err != nil {
+			return err
+		}
+
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				stakingtypes.EventTypeCompleteRedelegation,
+				sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
+				sdk.NewAttribute(stakingtypes.AttributeKeyDelegator, item.DelegatorAddress),
+				sdk.NewAttribute(stakingtypes.AttributeKeySrcValidator, item.ValidatorSrcAddress),
+				sdk.NewAttribute(stakingtypes.AttributeKeyDstValidator, item.ValidatorDstAddress),
+			),
+		)
+	}
+
+	return nil
+}
+
+func undelegateAllDelegations(ctx sdk.Context, k *stakingkeeper.Keeper, accAddr sdk.AccAddress) error {
+	for _, item := range k.GetAllDelegatorDelegations(ctx, accAddr) {
+		valAddr, err := sdk.ValAddressFromBech32(item.ValidatorAddress)
+		if err != nil {
+			return err
+		}
+
+		validator, found := k.GetValidator(ctx, valAddr)
+		if !found {
+			return fmt.Errorf("validator %s does not exist", valAddr)
+		}
+
+		completionTime, err := k.Undelegate(ctx, accAddr, valAddr, item.GetShares())
+		if err != nil {
+			return err
+		}
+
+		ctx.EventManager().EmitEvents(sdk.Events{
+			sdk.NewEvent(
+				stakingtypes.EventTypeUnbond,
+				sdk.NewAttribute(stakingtypes.AttributeKeyValidator, item.ValidatorAddress),
+				sdk.NewAttribute(sdk.AttributeKeyAmount, validator.TokensFromSharesTruncated(item.GetShares()).String()),
+				sdk.NewAttribute(stakingtypes.AttributeKeyDelegator, item.DelegatorAddress),
+				sdk.NewAttribute(stakingtypes.AttributeKeyCompletionTime, completionTime.Format(time.RFC3339)),
+			),
+		})
+	}
+
+	return nil
+}
+
+func completeAllUnbondingDelegations(ctx sdk.Context, k *stakingkeeper.Keeper, accAddr sdk.AccAddress, completionTime time.Time) error {
+	for _, item := range k.GetAllUnbondingDelegations(ctx, accAddr) {
+		for i := range item.Entries {
+			item.Entries[i].CompletionTime = completionTime
+		}
+
+		k.SetUnbondingDelegation(ctx, item)
+
+		valAddr, err := sdk.ValAddressFromBech32(item.ValidatorAddress)
+		if err != nil {
+			return err
+		}
+
+		balances, err := k.CompleteUnbonding(ctx, accAddr, valAddr)
+		if err != nil {
+			return err
+		}
+
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				stakingtypes.EventTypeCompleteUnbonding,
+				sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
+				sdk.NewAttribute(stakingtypes.AttributeKeyValidator, item.ValidatorAddress),
+				sdk.NewAttribute(stakingtypes.AttributeKeyDelegator, item.DelegatorAddress),
+			),
+		)
 	}
 
 	return nil
